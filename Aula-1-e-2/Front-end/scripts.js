db3ebdca56a2c3bcb359b0389da7fa2372aec1df
arkanorun <arkanorun@gmail.com>
2023-11-21 22:17:35 -0300

    adicao da api pokemon

--- Aula-1-e-2/Front-end/scripts.js
+++ Aula-1-e-2/Front-end/scripts.js
@@ -1,12 +1,26 @@
 //alert('fala galera') alert = manda uma mensagem de alerta no front (navegador)
-const tarefasHtml = document.getElementById('tarefas')
+const listaTarefasHtml = document.getElementById('tarefas');
+console.log(listaTarefasHtml);
 
-const chamadaApi = fetch('http://localhost:3000/tarefas')
-console.log(chamadaApi);
+// FETCH API - USADO PARA TRABALHAR COM REQUISICOES HTTP COM JAVASCRIPT (GET/POST/PUT/DELETE)
+// Primeiro parametro - End pont da requisicao (Para onde iremos enviar a nossa requisicao do front))
+//(https://localhost:3000/tarefas)
+// Segundo Parametro (opcional) - Configuraçoes da requisicao Ex: Metodo (GET/POST/PUT/DELETE)
 
-chamadaApi.then((response) => {
-    console.log(response)
+// FUNCAO ASSINCRONA (PROMISSE)
+const Api = fetch('http://localhost:3000/tarefas');
+console.log(Api);
+
+Api.then((response) => {
+    console.log(response);
     return response.json()
 }).then((tarefas) => {
-    console.log(tarefas)
+    console.log(tarefas);
+    tarefas.map((tarefa) => {
+        console.log(tarefa);
+        listaTarefasHtml.insertAdjacentHTML('beforeend', `<li>
+      <p>Tarefa: ${tarefa.text}</p>
+      <p>Prazo: ${tarefa.prazo}</p>
+    </li>`)
+    })
 })
